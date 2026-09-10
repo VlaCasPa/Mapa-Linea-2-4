3fdd87e925bd7fe6485afc6d7ccd7bb961062fe9
VlaCasPa <zebaxx@gmail.com>
2026-09-10 10:54:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const map = L.map('map').setView([-12.055, -77.050], 12);
 
-// Mapa Base: Colores Pasteles Suaves (CartoDB Voyager)
-L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; OpenStreetMap contributors &copy; CARTO',
-    subdomains: 'abcd',
-    maxZoom: 19,
-    opacity: 0.75, // <-- Mantenemos tu opacidad
-    className: 'mapa-tenue' // <-- Mantenemos tu filtro CSS
+// 2. Mapa Base: Google Maps (Monocromático Tenue)
+L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+    maxZoom: 20,
+    subdomains: ['mt0','mt1','mt2','mt3'],
+    attribution: '&copy; Google',
+    opacity: 0.65, // <-- Hace que todo el mapa se mezcle con el fondo
+    className: 'mapa-google-gris' // <-- Etiqueta para nuestro filtro de color
 }).addTo(map);
 
 const urlCSV = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSz_DsP2CT07FaYNRe4MIX7cO25I01gUb9e_aboGNrIHyBzHiVCX-Ea800l6R76rQ/pub?output=csv";
